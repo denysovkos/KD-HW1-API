28efe0bc839000cd9eae120ab120cac2d5003b31
Kostiantyn Denysov <denysov.kos@gmail.com>
2017-10-24 19:09:44 +0300

    added timeout

--- seed.js
+++ seed.js
@@ -8,11 +8,12 @@ db.isRecordsInBd()
     .then(dataFromDB => {
         if (!dataFromDB.length) {
             data.map(async hotel => {
-                await db.saveHotel(hotel);
+                await db.saveHotel(hotel)
             })
             console.log('Database was filled');
         } else {
             console.log('Data in db already exists');
         }
     })
+    .then(() => setTimeout(() => process.exit(0), 5000))
     .catch(err => console.log('error was happened: ', err));
