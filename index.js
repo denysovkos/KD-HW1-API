269984f00f23251957a7d810e15b74d12839210e
Kostiantyn Denysov <denysov.kos@gmail.com>
2017-10-24 10:18:58 +0300

    changed port

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const data = require('./MOCK_DATA');
 
 app.get('/hotels', function (req, res) {
     let random = data[Math.floor(Math.random() * data.length)];
+    console.log('API call. Will be returned: ', random);
     res.send(random);
 });
 
-app.listen(7777, function () {
-  console.log('KD-HW API RUNNING ON localhost:7777!')
+app.listen(7788, function () {
+  console.log('KD-HW API RUNNING ON localhost:7788!')
 });
