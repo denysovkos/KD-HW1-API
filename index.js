ab37a4ddcb7f89b5661e3eb68c5cec8746a44c9f
Kostiantyn Denysov <denysov.kos@gmail.com>
2017-10-24 18:16:47 +0300

    added mongo

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const data = require('./MOCK_DATA');
+const Database = require('./db');
+const db = new Database();
 
+db.init();
 
-app.get('/hotels', function (req, res) {
-    let random = data[Math.floor(Math.random() * data.length)];
-    console.log('API call. Will be returned: ', random);
-    res.send(random);
+app.get('/hotels', async (req, res) => {
+    let hotel = await db.getHotel();
+    console.log('API call. Will be returned: ', hotel);
+    res.send(hotel);
 });
 
 app.listen(7788, function () {
-  console.log('KD-HW API RUNNING ON localhost:7788!')
+  console.log('==> 🌎 KD-HW API RUNNING ON localhost:7788');
 });
---
+++ seed.js
@@ -0,0 +1,18 @@
+const data = require('./MOCK_DATA');
+const Database = require('./db');
+const db = new Database();
+
+db.init();
+
+db.isRecordsInBd()
+    .then(dataFromDB => {
+        if (!dataFromDB.length) {
+            data.map(async hotel => {
+                await db.saveHotel(hotel);
+            })
+            console.log('Database was filled');
+        } else {
+            console.log('Data in db already exists');
+        }
+    })
+    .catch(err => console.log('error was happened: ', err));
